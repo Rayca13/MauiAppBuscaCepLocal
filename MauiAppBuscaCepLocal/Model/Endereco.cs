fdfd4e0fa0410011ea5c36e46f70138052433545
Usuario <Usuario@CPD03-M06>
2024-09-02 12:30:36 -0300

    inicio

---
+++ MauiAppBuscaCepLocal/Model/Endereco.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppBuscaCepLocal.Model
+{
+    public class Endereco
+    {
+        public int Id_logradouro { get; set; }
+        public int Id_cidade { get; set; }
+        public string tipo { get; set; }
+        public string descricao { get; set; }
+        public string uf { get; set; }
+        public string complemento { get; set; }
+        public string descricao_sem_nome { get; set; }
+        public object rows { get; set; }
+        public int CEP{ get; set; }
+        public string UF { get; set; }
+    }
+}
---
+++ MauiAppBuscaCepLocal/Services/DataServiceCep.cs
@@ -0,0 +1,28 @@
+﻿using MauiAppBuscaCepLocal.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppBuscaCepLocal.Services
+{
+    public class DataServiceCep
+    {
+        public static async Task<Endereco?> GetEnderecoAsyncByCep (string Cep)
+        {
+            Endereco? end;
+
+            using (HttpClient client = new HttpClient ()) 
+            {
+                // 10.0.2.2
+                HttpResponseMessage response = await client.GetAsync("http://10.0.2.2:8000/endereco/by-cep?cep=" + cep);
+                
+                if (response.IsSuccessStatusCode) 
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+                }
+                
+            }
+        }
+}
